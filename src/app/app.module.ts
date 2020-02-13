51c3996361def67347743c6451fed854e8e16583
Dmitry <joydnby@gmail.com>
2020-02-13 21:24:57 +0300

    refactor: optimise components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
 import { SearchItemComponent } from './components/search-item/search-item.component';
-import { SearchFormComponent } from './components/header/search-form/search-form.component';
-import { SearchBoxComponent } from './components/header/search-box/search-box.component';
-import { LoginBoxComponent } from './components/header/login-box/login-box.component';
-import { SortingBoxComponent } from './components/header/sorting-box/sorting-box.component';
+import { SortingBoxComponent } from './components/sorting-box/sorting-box.component';
 
 @NgModule({
   declarations: [
@@ -17,9 +14,6 @@ import { SortingBoxComponent } from './components/header/sorting-box/sorting-box
     HeaderComponent,
     SearchResultsComponent,
     SearchItemComponent,
-    SearchFormComponent,
-    SearchBoxComponent,
-    LoginBoxComponent,
     SortingBoxComponent,
   ],
   imports: [
