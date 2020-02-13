31a64fc686aadc064af17d4a83f26bfde958f4bf
Dmitry <joydnby@gmail.com>
2020-02-13 21:24:57 +0300

    refactor: optimise components structure

--- src/app/components/search-results/search-results.component.ts
+++ src/app/components/search-results/search-results.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ISearchResponse } from '../../models/search-response.model';
+
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
