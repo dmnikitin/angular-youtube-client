6bfe735a200e980d562cb452d47432d197b10546
Dmitry <joydnby@gmail.com>
2020-02-15 12:07:48 +0300

    feat: add onsubmit method to header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { data } from '../../../assets/mockdata';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() public dataLoaded: EventEmitter<Event> = new EventEmitter();
+
   constructor() { }
 
   public ngOnInit(): void {
+  }
+
+  public onFormSubmit(event: Event): void {
+    event.preventDefault();
+    console.log('subm');
+    this.dataLoaded.emit(data);
   }
 
 }
