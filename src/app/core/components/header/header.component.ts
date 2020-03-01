ada0532be882078e44ff100228e31c5e3a24a79f
Dmitry <joydnby@gmail.com>
2020-03-01 16:23:42 +0300

    feat: add debounce

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,3 +1,4 @@
+import { debounceTime } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoadDataService } from '../../services/load-data.service';
@@ -14,16 +15,13 @@ export class HeaderComponent implements OnInit {
   constructor(private router: Router, private loadDataService: LoadDataService) { }
 
   public ngOnInit(): void {
+    this.loadDataService.searchQuery
+      .pipe(debounceTime(2000))
+      .subscribe((query: string) => this.loadDataService.getData(query));
   }
 
-  // public onFormSubmit(event: Event): void {
-  //   event.preventDefault();
-  //   this.loadDataService.onFormSubmit();
-  //   this.router.navigate(['videos']);
-  // }
-
-  public sendRequest(query: string): void {
-    this.loadDataService.getData(query);
+  public updateQuery(query: string): void {
+    this.loadDataService.searchQuery.next(query);
   }
 
   public toggleSortingBoxView(): void {
--- src/app/core/services/load-data.service.ts
+++ src/app/core/services/load-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Subject, from, timer, EMPTY } from 'rxjs';
-import { catchError, map, reduce, debounce, pluck, switchMap } from 'rxjs/operators';
+import { catchError, map, reduce, debounceTime, pluck, switchMap } from 'rxjs/operators';
 import { ISearchResponse } from '../../youtube/models/search-response.model';
 import { ISearchItem } from './../../youtube/models/search-item.model';
 
@@ -15,6 +15,7 @@ export class LoadDataService {
   set data(value: ISearchResponse) { this._data = value; }
 
   public dataObs: Subject<ISearchResponse> = new Subject<ISearchResponse>();
+  public searchQuery: Subject<string> = new Subject<string>();
 
   constructor(private http: HttpClient) { }
 
@@ -26,28 +27,26 @@ export class LoadDataService {
       .set('q', query)
       .set('type', 'video');
 
-    if (query.length > 7) {
-      this.http.get<ISearchResponse>(`${this.URL}search`, { params: searchParams })
-        .pipe(
-          pluck('items'),
-          switchMap((items: []) => from(items)),
-          map((item) => item.id.videoId),
-          reduce((totalScore, current) => totalScore + ',' + current),
-          switchMap((idString: string) => {
-            const videoParams: HttpParams = new HttpParams()
-              .set('part', 'snippet,statistics')
-              .set('id', idString);
-            return this.http.get<ISearchResponse>(`${this.URL}videos`, { params: videoParams });
-          }),
-          debounce(() => timer(10000)),
-          catchError((error) => {
-            console.log('error: ', error);
-            return EMPTY;
-          })
-        )
-        .subscribe((data: ISearchResponse) => {
-          this.dataObs.next(data);
-        });
-    }
+    this.http.get<ISearchResponse>(`${this.URL}search`, { params: searchParams })
+      .pipe(
+        pluck('items'),
+        switchMap((items: []) => from(items)),
+        map((item) => item.id.videoId),
+        reduce((totalScore, current) => totalScore + ',' + current),
+        switchMap((idString: string) => {
+          const videoParams: HttpParams = new HttpParams()
+            .set('part', 'snippet,statistics')
+            .set('id', idString);
+          return this.http.get<ISearchResponse>(`${this.URL}videos`, { params: videoParams });
+        }),
+        catchError((error) => {
+          console.log('error: ', error);
+          return EMPTY;
+        })
+      )
+      .subscribe((data: ISearchResponse) => {
+        this.dataObs.next(data);
+      });
+
   }
 }
