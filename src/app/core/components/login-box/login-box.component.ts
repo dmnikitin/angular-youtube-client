1a550369e32d702cfb186404ff8d7ee1e9c98920
Dmitry <joydnby@gmail.com>
2020-03-03 19:06:53 +0300

    refactor: use async pipe in login-box component

--- src/app/core/components/login-box/login-box.component.ts
+++ src/app/core/components/login-box/login-box.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { LoadDataService } from './../../services/load-data.service';
 import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
@@ -8,27 +7,20 @@ import { AuthService } from '../../../auth/services/auth.service';
   templateUrl: './login-box.component.html',
   styleUrls: ['./login-box.component.scss']
 })
-export class LoginBoxComponent implements OnInit, OnDestroy {
+export class LoginBoxComponent implements OnInit {
 
-  private componentDestroyed: Subject<boolean> = new Subject();
-  public userName: string = '';
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private loadDataService: LoadDataService) { }
 
   public ngOnInit(): void {
-    this.authService.userLoginObs
-      .pipe(takeUntil(this.componentDestroyed))
-      .subscribe((userName: string) => {
-        this.userName = userName;
-      });
   }
 
   public logout(): void {
     this.authService.logout();
-  }
-
-  public ngOnDestroy(): void {
-    this.componentDestroyed.next(true);
-    this.componentDestroyed.complete();
+    for (let prop in this.loadDataService.data) {
+      if (this.loadDataService.data.hasOwnProperty(prop)) {
+        delete this.loadDataService.data[prop];
+      }
+    }
   }
 
 }
