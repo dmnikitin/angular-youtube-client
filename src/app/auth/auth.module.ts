4cb4e4ad0e22800723a09f9eb74c7db79ad1f7c8
dmitry <joydnby@gmail.com>
2020-05-23 11:17:49 +0300

    refactor: login-page component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,18 +1,19 @@
-import { AuthRoutingModule } from './auth-routing.module';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from './../shared/shared.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { FormAuthComponent } from './components/form-auth/form-auth.component';
+import { GoogleAuthComponent } from './components/google-auth/google-auth.component';
 
 @NgModule({
-  declarations: [LoginPageComponent, SignupPageComponent],
+  declarations: [LoginPageComponent, FormAuthComponent, GoogleAuthComponent],
   imports: [
     CommonModule,
     FormsModule,
     SharedModule,
-    AuthRoutingModule,
+    RouterModule,
     ReactiveFormsModule,
   ],
   exports: [LoginPageComponent]
--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,5 @@
-import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
-import { AuthService as ASLService, GoogleLoginProvider } from 'angularx-social-login';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import { Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -12,48 +8,11 @@ import { Validators } from '@angular/forms';
 })
 export class LoginPageComponent implements OnInit {
 
-  public authForm: FormGroup;
+  public authType: number = 0;
 
-  constructor(
-    private social: ASLService,
-    public auth: AuthService,
-    private router: Router,
-    private fb: FormBuilder
-  ) { }
+  constructor(private route: ActivatedRoute) {}
 
   public ngOnInit(): void {
-    this.initForm();
-    this.auth.checkAuthentication().then((isAuthenticated: boolean) => {
-      if (isAuthenticated) {
-        this.router.navigate(['/videos']);
-      }
-    });
-    this.social.authState.subscribe((user) => {
-
-      // this.loggedIn = (user != null);
-      if (user) {
-        this.auth.provideAuth(user.firstName);
-        this.router.navigate(['/videos']);
-      } else {
-        this.router.navigate(['']);
-      }
-    });
+    this.authType = this.route.snapshot.data.authType;
   }
-
-  public initForm(): void {
-    this.authForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(4)]],
-      password: ['', [Validators.required, Validators.minLength(4)]]
-    });
-  }
-
-  public onLoginAttempt(): void {
-    const { name, password } = this.authForm.value;
-    this.auth.login(name, password);
-  }
-
-  public signInWithGoogle(): void {
-    this.social.signIn(GoogleLoginProvider.PROVIDER_ID);
-  }
-
 }
