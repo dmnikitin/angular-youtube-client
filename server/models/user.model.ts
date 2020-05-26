020a18dedf24c47e264aa16e340f4eb38b9a6e37
dmitry <joydnby@gmail.com>
2020-05-26 14:56:35 +0300

    feat: add user model

---
+++ server/models/user.model.ts
@@ -0,0 +1,21 @@
+import * as mongoose from 'mongoose';
+import hash from '../helpers/hash';
+import { v4 as uuidv4 } from 'uuid';
+
+const userSchema: mongoose.Schema = new mongoose.Schema(
+  {
+    _id: { type: String, default: uuidv4() },
+    login: { type: String },
+    password: { type: String }
+  },
+  { versionKey: false }
+);
+
+userSchema.statics.toResponse = (user) => {
+  const { id, login } = user;
+  return { id, login };
+};
+
+userSchema.pre('save', hash);
+
+export const userModel: any = mongoose.model('User', userSchema);
