25b09d99fcf484f556d3450aa889d8587745813b
dmitry <joydnby@gmail.com>
2020-05-31 14:09:31 +0300

    fix: accidental router remove from auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
-import { AuthGuard } from './core/guards/auth.guard';
+import { AuthGuard } from './auth/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from './../shared/shared.module';
@@ -13,6 +14,7 @@ import { GoogleAuthComponent } from './components/google-auth/google-auth.compon
     FormsModule,
     SharedModule,
     ReactiveFormsModule,
+    RouterModule
   ],
   exports: [LoginPageComponent]
 })
