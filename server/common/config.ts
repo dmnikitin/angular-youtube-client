f50b12f64f2f6c180e2ca9837224483639ff14f5
dmitry <joydnby@gmail.com>
2020-06-01 14:19:31 +0300

    refactor: API_KEY to BE config from FE

--- server/common/config.ts
+++ server/common/config.ts
@@ -9,12 +9,14 @@ interface IConfig {
   PORT: string;
   MONGO_CONNECTION_STRING: string;
   JWT_SECRET_KEY: string;
+  API_KEY: string;
 }
 
 const config: IConfig = {
   PORT: process.env.PORT,
   MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  API_KEY: process.env.API_KEY,
 };
 
 export default config;
